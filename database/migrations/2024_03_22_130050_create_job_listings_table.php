ad484dfe85fc62e4fb44186edd9440ae80461869
Md Shariful Haque <sharifulhaque786@gmail.com>
2024-03-26 21:24:58 +0400

    migration modified

--- database/migrations/2024_03_22_130050_create_job_listings_table.php
+++ database/migrations/2024_03_22_130050_create_job_listings_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration {
     /**
      * Run the migrations.
@@ -13,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            //            $table->unsignedBigInteger('employer_id');
             $table->foreignIdFor(\App\Models\Employer::class);
             $table->string('title');
             $table->string('salary');
