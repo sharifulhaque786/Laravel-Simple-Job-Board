9883fcc96de00012868f654c32b4d7285935741a
Md Shariful Haque <sharifulhaque786@gmail.com>
2025-01-14 18:40:27 +0400

    new emplyoer info with blog post

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Employer extends Model
 {
+
     use HasFactory;
-    protected $fillable = ['title', 'content'];
 }
